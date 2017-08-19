8085a442b7407e47009780285aadf8e384ef19cb
mocha <mocha2007andcoco@gmail.com>
2017-08-19 17:25:29 -0400

    added 26 suggestions from Leoshi

--- space.py
+++ space.py
@@ -40,10 +40,11 @@ def isstar():
 def rname():
 	syllables=['mer','cu','ry','ve','nus','ter','ra','ju','pi','sa','turn','u','ra','nep','tun','ha','tsu','ne','mi','ku']
 	return random.choice(syllables)+random.choice(syllables)
-def rshipname():
+def rshipname():#48 names total
 	fict=['Endurance','Enterprise','Hedgehog','Liberty','Nostromo','Orville','Protector','Radiant Star','Serenity','Voyager']#Spacecraft in fictional works, and also the liberty which was the placeholder name
 	fact=['Atlantis','Blizzard','Discovery','Eagle','Endeavour','Falcon','Intrepid','Jules Verne','Odyssey','Orion','Unity']#IRL spacecraft, with Blizzard for Buran
 	navy=['Beagle']#boat names
+	leoshi=['Illumination','Journey','Destiny','Zenith','Nadir','Inner Circle','Arctic','Indespensible','Oversight','Aurora','Sensibility','Matchbox','Genesis','Terra','Order','Titan','Immortality','Singularity','Fusion','Normandy','Pantheon','Duke','Crossroads','Symphony','Star Maid','Horizon']
 	return random.choice(fict+fact+navy)
 def roman(n):
 	if n<11:return ['0','I','II','III','IV','V','VI','VII','VIII','IX','X'][n]
@@ -197,7 +198,7 @@ def launch(shipyard):
 	fleetnames=[]
 	for vessel in fleet:
 		fleetnames+=[vessel[0]]
-	numeral=len(fleetnames)//22#22 = number of unique names
+	numeral=len(fleetnames)//48#48 = number of unique names
 	name=rshipname()
 	while name in fleetnames:
 		name=rshipname()
